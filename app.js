1f47adb3d86518d067e1d0b6c333eb8ecebaac15
ismetcanbyk <ismetcanbyk@gmail.com>
2023-02-24 14:41:22 +0300

    files added

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+
+//Template Engine 
+app.set("view engine", "ejs");
+
+
+//Middlewares
+app.use(express.static("public"));
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.status(200).render('index', {
+        page_name: "index"
+    });
+});
+
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about"
+    });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`App started on port ${port}.`);
+});
